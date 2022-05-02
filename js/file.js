77aca9ff119ad9ce37ec8953cccffeecd476869d
= <saram.chaudharyaman@gmail.com>
2022-05-02 18:23:35 +0530

    fun

---
+++ js/file.js
@@ -0,0 +1,18 @@
+const yes = document.querySelector('.yes')
+const no = document.querySelector('.no')
+const question = document.querySelector('.question')
+const result = document.querySelector('.result')
+no.addEventListener("click", () => {
+    let arrTop = ["2em", "6em"]
+    let arrLeft = ["1em", "5em"]
+    const ranTop = Math.floor(Math.random() * arrTop.length)
+    const ranNumTop = (ranTop, arrTop[ranTop])
+    const ranLeft = Math.floor(Math.random() * arrLeft.length)
+    const ranNumLeft = (ranLeft, arrLeft[ranLeft])
+    no.style.left = ranNumLeft
+    no.style.top =  ranNumTop
+})
+yes.addEventListener("click", () => {
+    question.style.display = "none"
+    result.style.display = "block"
+})
